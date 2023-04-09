18ca07490ffe370eec8ef4034280f502a2b4f92a
OlegKhudoerko <110164546+OlegKhudoerko@users.noreply.github.com>
2023-04-09 23:53:49 +0300

    Решение 3 примера домашнего задания 2 семинара

---
+++ src/HW_2/Example_3.java
@@ -0,0 +1,30 @@
+package HW_2;
+
+import java.util.Arrays;
+
+public class Example_3 {
+    public static void main(String[] args){
+
+        int a = 90;
+        int b = 3;
+        int[] abc = new int[]{1, 2};
+        printSum(23, 234);
+
+        try {
+            System.out.println(a / b);
+            abc[3] = 9;
+            System.out.println(Arrays.toString(abc));
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+
+    public static void printSum(Integer a, Integer b) {//Экономичнее: public static void printSum(int a, int b)
+        System.out.println(a + b);
+    }
+
+}
